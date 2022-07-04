2bf0aaf65225760d6c5637bd0eb955ce4150fcb3
Ugur Gurler <108265528+CtTaeUG@users.noreply.github.com>
2022-07-04 21:40:13 +0300

    İlk işlemler yapıldı

---
+++ Mr.Ugurg/SDET/Java/Practice/src/Deneme.java
@@ -0,0 +1,18 @@
+public class Deneme {
+
+    public static void main(String[] args) {
+        int x = 5, y = 5;
+
+        Integer i = 10;
+        int a = 10;
+
+        System.out.println("i = " + i);
+
+
+    }
+
+
+
+
+
+}
